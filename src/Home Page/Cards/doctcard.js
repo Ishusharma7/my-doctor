2b133015f784fb268b6478a89963f91b9fe6ffda
“[Ishusharma7]” <sharmaishu12356@gmail.com>
2023-08-28 02:19:31 +0530

    danse

--- src/Home Page/Cards/doctcard.js
+++ src/Home Page/Cards/doctcard.js
@@ -1,37 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import css from './doc.module.css'
+import css from './doc.module.css';
 import Button from '@mui/material/Button';
 
 function Doc() {
-  const  data= [
-        {h1: 'Dr. Dusty Huel', p:'Master in Cardiovascular Risk Factors | Bachelor of Dental Surgery | Bachelor of Medicine Critical Care Medicine 8 years experience', hospital: 'Not avaliable', lang: 'Hindi,Dogri,Kashmiri', aval:'Not available'},
-        {h1: 'Dr. Dusty Huel', p:'Master in Cardiovascular Risk Factors | Bachelor of Dental Surgery | Bachelor of Medicine Critical Care Medicine 8 years experience', hospital: 'Not avaliable', lang: 'Hindi,Dogri,Kashmiri', aval:'Not available'},
-        {h1: 'Dr. Dusty Huel', p:'Master in Cardiovascular Risk Factors | Bachelor of Dental Surgery | Bachelor of Medicine Critical Care Medicine 8 years experience', hospital: 'Not avaliable', lang: 'Hindi,Dogri,Kashmiri', aval:'Not available'},
-        {h1: 'Dr. Dusty Huel', p:'Master in Cardiovascular Risk Factors | Bachelor of Dental Surgery | Bachelor of Medicine Critical Care Medicine 8 years experience', hospital: 'Not avaliable', lang: 'Hindi,Dogri,Kashmiri', aval:'Not available'},
-        {h1: 'Dr. Dusty Huel', p:'Master in Cardiovascular Risk Factors | Bachelor of Dental Surgery | Bachelor of Medicine Critical Care Medicine 8 years experience', hospital: 'Not avaliable', lang: 'Hindi,Dogri,Kashmiri', aval:'Not available'},  
-        {h1: 'Dr. Dusty Huel', p:'Master in Cardiovascular Risk Factors | Bachelor of Dental Surgery | Bachelor of Medicine Critical Care Medicine 8 years experience', hospital: 'Not avaliable', lang: 'Hindi,Dogri,Kashmiri', aval:'Not available'},
-        {h1: 'Dr. Dusty Huel', p:'Master in Cardiovascular Risk Factors | Bachelor of Dental Surgery | Bachelor of Medicine Critical Care Medicine 8 years experience', hospital: 'Not avaliable', lang: 'Hindi,Dogri,Kashmiri', aval:'Not available'}
-    ]
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('http://my-doctors.net:8090/doctors');
+        const data = response.data.data; // Accessing the 'data' array from the response
+        setDoctors(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className={css.a}>
-        {data.map((item, index) => (
-            <div  className={css.both} key={index}>
-              <div><AccountCircleIcon  sx={{ fontSize: {xs:'50px',md:'150px'}, marginTop: '.5em' }} /></div>
-              <div className={css.up}>
-                <h1>{item.h1}</h1>
-                <p>{item.p}</p>
-                <div className={css.low}>
-                <div className={css.lower}><h2>Hospital</h2><p>{item.hospital}</p></div>
-                <div className={css.lower}><h2>Languages</h2><p>{item.lang}</p></div>
-                <div className={css.lower}><h2>Next available</h2><p>{item.aval}</p></div>
-                </div>
-                <Button variant="outlined" sx={{width: {xs:'150px',md:'300px'}, fontSize: {xs:'10px',md:'25px'}, height: {xs:'30px', md:'60px'}, borderRadius: '20px', color: '#3f51b5', borderColor: '#3f51b5'}}>BOOK APOINTMENT</Button>
+      {doctors.map((item, index) => (
+        <div className={css.both} key={index}>
+          <div>
+            <AccountCircleIcon
+              sx={{ fontSize: { xs: '50px', md: '150px' }, marginTop: '.5em' }}
+            />
+          </div>
+          <div className={css.up}>
+            <h1>{`${item.firstName} ${item.lastName}`}</h1>
+            <p>
+              {item.profile?.qualifications?.map((qualification) => qualification.name).join(' | ')}
+              
+              {item.profile?.specialities?.map((speciality) => speciality.name).join(', ')}
+            </p>
+
+            <div className={css.low}>
+              <div className={css.lower}>
+                <h2>Hospital</h2>
+                <p>{/* hospi type here */}</p>
+              </div>
+              <div className={css.lower}>
+                <h2>Languages</h2>
+                <p>{item.profile?.languages?.length ? item.profile.languages.join(', ') : 'Not available'}</p>
+              </div>
+              <div className={css.lower}>
+                <h2>Next available</h2>
+                <p>Not available</p>
               </div>
             </div>
-          ))}
+            <Button
+              variant="outlined"
+              sx={{
+                width: { xs: '150px', md: '320px' },
+                fontSize: { xs: '10px', md: '25px' },
+                height: { xs: '30px', md: '60px' },
+                borderRadius: '20px',
+                color: '#3f51b5',
+                borderColor: '#3f51b5',
+                marginTop:'auto',
+              }}
+            >
+              BOOK APPOINTMENT
+            </Button>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Doc
+export default Doc;
--- src/Home Page/Cards/specialities.js
+++ src/Home Page/Cards/specialities.js
@@ -1,20 +1,35 @@
 import React from 'react'
-import e from '../../Login Page/images/e.svg'
+import { useEffect, useState } from 'react';
 import css from './specialities.module.css'
 import Left from './leftbar'
 
 function Special() {
-   const data=[
-        {src: e, para: "Bone Marrow" },
-        {src: e, para: "Bone Marrow" },
-        {src: e, para: "Bone Marrow" },
-        {src: e, para: "Bone Marrow" },
-        {src: e, para: "Bone Marrow" },
-        {src: e, para: "Bone Marrow" },
-        {src: e, para: "Bone Marrow" },
-        {src: e, para: "Bone Marrow" },
-        {src: e, para: "Bone Marrow" }
-    ]
+  const [specialData, setSpecialData] = useState([]);
+  const getSpecializationData = async () => {
+    try {
+      const response = await fetch(
+        "http://my-doctors.net:8090/specializations"
+      );
+      let data = await response.json();
+      let returnedData = data.data;
+      let totalSpecializations = data.total;
+      console.log(totalSpecializations);
+      console.log(returnedData);
+      const details = [];
+      for (let elem in returnedData) {
+        details.push({
+          name: returnedData[elem]["name"],
+          image: returnedData[elem]["imageUrl"],
+          totalSpecializations: Math.floor(totalSpecializations / 10),
+        });
+      }
+      setSpecialData(details);
+    } catch (error) {
+    }
+  };
+  useEffect(() => {
+    getSpecializationData();
+  }, []);
   return (
     <div className={css.he}>
     <div className={css.l}>
@@ -36,13 +51,13 @@ function Special() {
 </select>
         </div>
     <div className={ css.main}>
-        {data.map((item, index) => (
+        {specialData.map((item, index) => (
             <div className={css.image} key={index}>
               <img
-               src={item.src} 
+               src={`http://my-doctors.net/${item.image}`} 
                 width={200}
                />
-              <p>{item.para}</p>
+              <p>{item.name}</p>
             </div>
           ))}
     </div>
